cdfdb88855542b4a627a1160fddf0544eb63e048
Mark Lindsay <mark@octopart.com>
2020-01-29 15:12:23 +0000

    Encourage the use of nvim

--- create-symbolic-links.bash
+++ create-symbolic-links.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+#ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 mkdir -p $HOME/.config/nvim
 ln -s $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
 ln -s $HOME/dotfiles/coc-settings.json $HOME/.config/nvim/coc-settings.json
